da9872f2b3fd1cd600fff29e0f73c742925a26d2
wilson <wilsongeorge1996@gmail.com>
2022-06-04 15:05:34 +0530

    Route the Shop component, Create ProductsContext to store all the products

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 
-import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
+import Home from "./routes/home/home.component";
 import Authentication from './routes/authentication/authentication.component';
+import Shop from './routes/shop/shop.component';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path='shop' />
+        <Route path='shop' element={<Shop />} />
         <Route path='auth' element={<Authentication />} />
       </Route>
     </Routes>
