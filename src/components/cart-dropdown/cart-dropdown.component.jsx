25fe1c4af5a36f03310f224317113184627f7ee3
wilson <wilsongeorge1996@gmail.com>
2022-06-06 01:11:28 +0530

    Create the Checkout Route

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { CartContext } from '../../contexts/cart.context';
 
@@ -8,7 +9,13 @@ import CartItem from '../cart-item/cart-item.component';
 import './cart-dropdown.styles.scss';
 
 function CartDropdown() {
-   const { cartItems } = useContext(CartContext);
+   const { cartItems, setIsCartOpen } = useContext(CartContext);
+   const navigate = useNavigate();
+
+   const goToCheckoutHandler = () => {
+      navigate('/checkout');
+      setIsCartOpen(false);
+   }
 
    return (
       <div className='cart-dropdown-container'>
@@ -17,7 +24,7 @@ function CartDropdown() {
                <CartItem key={item.id} item={item} />
             ))}
          </div>
-         <Button>Go to Checkout</Button>
+         <Button onClick={goToCheckoutHandler}>Go to Checkout</Button>
       </div>
    );
 }
---
+++ src/routes/checkout/checkout.component.jsx
@@ -0,0 +1,13 @@
+import CheckoutItems from "../../components/checkout-items/checkout-items.component";
+
+import './checkout.styles.scss';
+
+function Checkout() {
+   return (
+      <div>
+         <CheckoutItems />
+      </div>
+   );
+}
+
+export default Checkout;
