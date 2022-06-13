af686f468fa9f3ae47c3c24e2c9d381838dbaba6
wilson <wilsongeorge1996@gmail.com>
2022-06-14 00:51:59 +0530

    Link CategoryItems in the homepage to the corresponding Category Route

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,11 +1,16 @@
+import { useNavigate } from 'react-router-dom';
+
 import './category-item.styles.jsx';
 import { CategoryItemContainer, BackgroundImage, Body } from './category-item.styles.jsx';
 
 function CategoryItem({ category }) {
-   const { imageUrl, title } = category;
+   const { imageUrl, title, route } = category;
+   const navigate = useNavigate();
+
+   const onNavigateHandler = () => navigate(route);
 
    return (
-      <CategoryItemContainer>
+      <CategoryItemContainer onClick={onNavigateHandler}>
          <BackgroundImage imageUrl={imageUrl} />
          <Body>
             <h2>{title.toUpperCase()}</h2>
