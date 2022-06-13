73c5ce5121b52d1bdd778b77f8b6aed77a98c1cd
wilson <wilsongeorge1996@gmail.com>
2022-06-13 14:07:58 +0530

    Slight tweak to the CategoryItem component style

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -9,7 +9,7 @@ function CategoryItem({ category }) {
             { backgroundImage: `url(${imageUrl})` }
          } />
          <div className="category-body-container">
-            <h2>{title}</h2>
+            <h2>{title.toUpperCase()}</h2>
             <p>Shop Now</p>
          </div>
       </div>
