6140074f7d486bb916c1133d6df361aff94e475d
wilson <wilsongeorge1996@gmail.com>
2022-06-25 23:49:37 +0530

    Add redux-persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,31 @@
 import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from '@reduxjs/toolkit';
+
 import userReducer from './user/user-slice';
 import categoriesReducer from './categories/categories-slice';
 import cartReducer from './cart/cart-slice';
 
+
+const rootReducer = combineReducers({
+   user: userReducer,
+   categories: categoriesReducer,
+   cart: cartReducer
+});
+
+const persistConfig = {
+   key: 'root',
+   storage,
+   blacklist: ['user']
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-   reducer: {
-      user: userReducer,
-      categories: categoriesReducer,
-      cart: cartReducer
-   },
+   reducer: persistedReducer,
    middleware: [logger]
 });
+
+export const persistor = persistStore(store);
